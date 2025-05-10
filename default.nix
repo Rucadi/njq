ebd58b6fbf47fc1235663ab5f94315a8a08d3b91
ruben <ruben.cano96@gmail.com>
2025-05-10 09:14:24 +0200

    Update cargoHash

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@
   }) {}
 }:pkgs.rustPlatform.buildRustPackage rec {
   pname = "njq";
-  version = "0.0.2";
-  cargoHash = "sha256-Z4whDevtbS1cmkrgdASmTTJbGANPaQWJODK9djgp2QY=";
+  version = "0.0.3";
+  cargoHash = "sha256-tDz9+iQhutlo7petKmg6n/mg0tDntGRqwBALcATJwdM=";
   src = pkgs.lib.cleanSource ./.;
 }
